95a3c683a64116a767e270087d94d7ae479ee5f2
Muhammad Usama Riaz <82031844+HafizUsamaRiaz@users.noreply.github.com>
2026-04-13 01:22:43 +0500

    Update Google Client ID and Sheet URL

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { SECTION_A, SECTION_B, SECTION_C } from "./questions.js";
 // ═══════════════════════════════════════════════════════════════
 //  🔧 CONFIGURATION
 // ═══════════════════════════════════════════════════════════════
-const GOOGLE_CLIENT_ID = "https://script.google.com/macros/s/AKfycbzGos0nqk-BIjx9zwuVtSRfU4diIbfGwlKTiBZhcQfDiG25gNPxdaiW0d5dxgrG9Qqkhw/exec";
+const GOOGLE_CLIENT_ID = "807519902454-pir1e3qaa9kfb7et1o4dh8t8dtsaouil.apps.googleusercontent.com";
 const ITU_DOMAIN       = "itu.edu.pk";
-const SHEET_URL        = "YOUR_APPS_SCRIPT_URL_HERE";
+const SHEET_URL        = "https://script.google.com/macros/s/AKfycbzGos0nqk-BIjx9zwuVtSRfU4diIbfGwlKTiBZhcQfDiG25gNPxdaiW0d5dxgrG9Qqkhw/exec";
 const COURSE           = "CE203L · Computer Organization & Architecture Lab";
 const EXAM_TITLE       = "Midterm Exam";
 const EXAM_SUB         = "Labs 1–6 · Verilog HDL & Digital Design";
